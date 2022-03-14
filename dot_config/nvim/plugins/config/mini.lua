854e717e16f76be715753e9614f7a1ce4cd8d23c
turtton <watagame2579@gmail.com>
2022-03-14 11:26:07 +0900

    add mini.sessions config

--- dot_config/nvim/plugins/config/mini.lua
+++ dot_config/nvim/plugins/config/mini.lua
@@ -59,7 +59,23 @@ require('mini.pairs').setup({
 })
 -- ===== session management ======
 require('mini.sessions').setup({
-	autoread = true
+  -- Whether to read latest session if Neovim opened without file arguments
+  autoread = false,
+
+  -- Whether to write current session before quitting Neovim
+  autowrite = true,
+
+  -- Directory where global sessions are stored (use `''` to disable)
+  directory = '',--<"session" subdir of user data directory from |stdpath()|>,
+
+  -- File for local session (use `''` to disable)
+  file = '',
+
+  -- Whether to force possibly harmful actions (meaning depends on function)
+  force = { read = false, write = true, delete = false },
+
+  -- Whether to print session path after action
+  verbose = { read = false, write = true, delete = true },
 })
 -- ===== show start screen ======
 require('mini.starter').setup({})
