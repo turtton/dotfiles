4879c30d9761c4dce706542d0c008b110a00cc6e
turtton <top.gear7509@turtton.net>
2022-12-19 22:25:32 +0900

    fix language server name
    Signed-off-by: turtton <top.gear7509@turtton.net>

--- dot_config/nvim/plugins/config/lspins.lua
+++ dot_config/nvim/plugins/config/lspins.lua
@@ -4,24 +4,25 @@ local lspconfig = require("lspconfig")
 
 -- Settings
 mason.setup({
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
-    }
+	ui = {
+		icons = {
+			server_installed = "✓",
+			server_pending = "➜",
+			server_uninstalled = "✗"
+		}
+	}
 })
 
 mason_lspconfig.setup({
-	ensure_installed = {'sumneko_lua', 'vimls', 'bashls', 'nimls', 'taplo', 'yamlls', 'tsserver', 'jsonls', 'kotlin_language_server', 'rust_analyzer', 'clangd'},
+	ensure_installed = { 'sumneko_lua', 'vimls', 'bashls', 'nimlsp', 'taplo', 'yamlls', 'tsserver', 'jsonls',
+		'kotlin_language_server', 'rust_analyzer', 'clangd' },
 	atomatic_installation = { exclude = { "rust_analyzer" } },
 })
 
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
@@ -30,25 +31,26 @@ vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<C
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local function on_attach_func(_, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-	local opts = { noremap=true, silent=true }
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format { async = true }<CR>', opts)
+	local opts = { noremap = true, silent = true }
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl',
+		'<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format { async = true }<CR>', opts)
 end
 
 mason_lspconfig.setup_handlers({
@@ -58,28 +60,28 @@ mason_lspconfig.setup_handlers({
 		capabilities.textDocument.completion.completionItem.snippetSupport = true
 		local opts = {
 			on_attach = on_attach_func,
-		  capabilities = capabilities
-	 }
-
-	 if server_name == "sumneko_lua" then
-		 opts.Lua = {
-			 hint= {
-				 enable = true,
-			},
-			diagnostics = {
-				globals = {'vim'},
-			},
-			telemetry = {
-				enable = false,
-			},
+			capabilities = capabilities
 		}
-	 end
 
-	 if server_name == "rust_analyzer" then
-		 local rt = require("rust-tools")
-		 rt.setup {
+		if server_name == "sumneko_lua" then
+			opts.Lua = {
+				hint = {
+					enable = true,
+				},
+				diagnostics = {
+					globals = { 'vim' },
+				},
+				telemetry = {
+					enable = false,
+				},
+			}
+		end
+
+		if server_name == "rust_analyzer" then
+			local rt = require("rust-tools")
+			rt.setup {
 				server = {
-					on_attach = function (any, bufnr)
+					on_attach = function(any, bufnr)
 						on_attach_func(any, bufnr)
 						vim.keymap.set("n", "<Leader>A", rt.hover_actions.hover_actions, { buffer = bufnr })
 						vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
@@ -88,12 +90,12 @@ mason_lspconfig.setup_handlers({
 			}
 			-- server:attach_buffers()
 			-- require("rust-tools").start_standalone_if_required()
-	 else
-		server.setup(opts)
-	 end
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
+		else
+			server.setup(opts)
+		end
+		-- (optional) Customize the options passed to the server
+		-- if server.name == "tsserver" then
+		--     opts.root_dir = function() ... end
+		-- end
 	end
 })
