0c5b005be87a45c8e6a02a70d3ebeb7fda9621a6
turtton <tiny.idea1859@turtton.net>
2022-12-19 16:45:32 +0900

    add packer version plugin script
    Signed-off-by: turtton <tiny.idea1859@turtton.net>

--- dot_config/nvim/plugins/packer.lua
+++ dot_config/nvim/plugins/packer.lua
@@ -38,8 +38,102 @@ return require('packer').startup(function(use)
 	}
 
 	-- comp --
+	use {
+		'Shougo/ddc.vim',
+		config = "vim.cmd[[exec 'source' . g:PLUGIN_CONFIG_DIR . '/ddc.vim']]",
+		requires = {
+			{ 'vim-denops/denops.vim' },
+			{ 'Shougo/ddc-ui-native' },
+			{ 'Shougo/ddc-nvim-lsp' },
+			{ 'Shougo/ddc-around' },
+			{ 'Shougo/ddc-matcher_head' },
+			{ 'Shougo/ddc-sorter_rank' },
+			{ 'Shougo/ddc-converter_remove_overlap' },
+			{ 'Shougo/ddc-zsh' },
+			{
+				'vim-skk/skkeleton',
+				config = "vim.cmd[[exec 'source' . g:PLUGIN_CONFIG_DIR . '/skkeleton.vim']]"
+			},
+			{
+				'matsui54/denops-popup-preview.vim',
+				requires = { 'Shougo/pum.vim' },
+				config = "vim.fn['popup_preview#enable']()"
+			},
+			{
+				'matsui54/denops-signature_help',
+				config = "vim.fn['signature_help#enable']()"
+			}
+		}
+	}
 
+	-- language tools --
+	use {
+		'scalameta/nvim-metals',
+		opt = true,
+		ft = { 'scala', 'sbt' },
+		requires = { 'nvim-lua/plenary.nvim' }
+	}
+	use {
+		'simrat39/rust-tools.nvim',
+	--	opt = true,
+	--	ft = { 'rs', 'toml' },
+		requires = {
+			{ 'mfussenegger/nvim-dap' },
+			{
+				'saecki/crates.nvim',
+		requires = { 'nvim-lua/plenary.nvim' },
+				config = function() require('crates').setup() end
+			}
+		}
+	}
+	use {
+		'peterhoeg/vim-qml',
+		opt = true,
+		ft = { 'qml' }
+	}
+	use { 'normen/vim-pio' }
 
+	-- syntax highlight --
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate',
+		config = "vim.cmd[[exec 'source' . g:PLUGIN_CONFIG_DIR . '/treesitter.lua']]"
+	}
+
+	-- git --
+	use {
+		'lewis6991/gitsigns.nvim',
+		config = "vim.cmd[[exec 'source' . g:PLUGIN_CONFIG_DIR . '/gitsigns.lua']]"
+	}
+
+	-- utils --
+	-- file tree
+	use {
+		'lambdalisue/fern.vim',
+		config = "vim.cmd[[nnoremap <silent> wf :Fern . -drawer -reveal=%<CR>]]"
+	}
+	-- key documentation
+	use {
+		'folke/which-key.nvim',
+		config = "vim.cmd[[exec 'source' . g:PLUGIN_CONFIG_DIR . '/which-key.lua']]"
+	}
+	-- improve asterisk searching
+	use {
+		'haya14busa/vim-asterisk',
+		config = "vim.cmd[[exec 'source' . g:PLUGIN_CONFIG_DIR . '/asterisk.vim']]"
+	}
+	-- mini(huge) functional utils
+	use {
+		'echasnovski/mini.nvim',
+		branch = 'stable',
+		config = "vim.cmd[[exec 'source' . g:PLUGIN_CONFIG_DIR . '/mini.lua']]"
+	}
+	-- fuzzy finder
+	use {
+		'nvim-telescope/telescope.nvim',
+		config = "vim.cmd[[exec 'source' . g:PLUGIN_CONFIG_DIR . '/telescope.vim']]",
+		requires = { 'nvim-telescope/telescope-ui-select.nvim' }
+	}
 
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
